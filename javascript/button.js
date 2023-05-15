28ba1e819ca7e9bf494d14d72817efcdb597a7f3
anonCantCode <133663594+anonCantCode@users.noreply.github.com>
2023-05-15 18:12:06 +0530

    Create button.js

---
+++ javascript/button.js
@@ -0,0 +1,58 @@
+async function waitForWebUiUpdate(divToWatch) {
+	const promise = new Promise((resolve, reject) => {
+		const iframes = divToWatch.querySelectorAll("iframe");
+		if (iframes.length > 0) {
+			resolve();
+			return;
+		}
+		const mutationConfig = { attributes: true, childList: true, subtree: true };
+		const onMutationHappened = (mutationList, observer) => {
+			observer.disconnect();
+			resolve();
+		};
+		const observer = new MutationObserver(onMutationHappened);
+		observer.observe(divToWatch, mutationConfig);
+	});
+	return await promise;
+}
+
+async function setImageOnInput(imageInput, img) {
+	const response = await fetch(img);
+	const blob = await response.blob();
+	const file = new File([blob], "image.png", { type: "image/png" });
+	const dt = new DataTransfer();
+	dt.items.add(file);
+	const list = dt.files;
+	imageInput.files = list;
+	const event = new Event("change", {
+		"bubbles": true,
+		"composed": true
+	});
+	imageInput.dispatchEvent(event);
+}
+
+function sendImageToControlNet(from_tab, to_tab, control_unit) {
+	let gallery = txt2img_gallery;
+	let tabId = "#txt2img_script_container";
+	if (from_tab === "img2img") {
+		gallery = img2img_gallery;
+	}
+	if (to_tab === "img2img") {
+		tabId = "#img2img_script_container";
+	}
+	const img = gallery.querySelectorAll("img")[0].src;
+	const controlNetDiv = gradioApp().querySelector(tabId).querySelector("#controlnet");
+	var iframes = controlNetDiv.querySelectorAll("iframe");
+	if (iframes.length == 0) {
+		controlNetDiv.querySelector("span.icon").click();
+	}
+	waitForWebUiUpdate(controlNetDiv).then(() => {
+		const tabs = controlNetDiv.querySelectorAll("div.tab-nav > button");
+		if(tabs !== null && tabs.length > 1) {
+			tabs[control_unit].click();
+		}
+		imageInput = controlNetDiv.querySelectorAll("input[type='file']")[0];
+		setImageOnInput(imageInput, img);
+	}
+	);
+}
