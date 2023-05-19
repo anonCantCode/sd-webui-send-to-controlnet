e98634e73ea29776c3dcddc39b3beff2ccc7f6cf
anonCantCode <133663594+anonCantCode@users.noreply.github.com>
2023-05-20 02:38:05 +0530

    Update main.py: Improve button positions

--- scripts/main.py
+++ scripts/main.py
@@ -1,27 +1,38 @@
 from modules import extensions
+from modules import script_callbacks
 import modules.scripts as scripts
+from modules.shared import opts
 import gradio as gr
 
 
 class Script(scripts.Script):
-	def __init__(self) -> None:
-		super().__init__()
-
 	def title(self):
 		return "SendToControlNet"
 
 	def show(self, is_img2img):
 		return scripts.AlwaysVisible
 
-	def ui(self, is_img2img):
-		for extension in extensions.active():
-			if "controlnet" in extension.name:
-				controlNetButtonTxToTx = gr.Button(value="Send to ControlNet #0 (txt2img -> txt2img)", elem_id="sendto_controlnet_button_tx_to_tx")
-				controlNetButtonTxToTx.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'txt2img', 0)}")
-				controlNetButtonImToIm = gr.Button(value="Send to ControlNet #0 (img2img -> img2img)", elem_id="sendto_controlnet_button_im_to_im")
-				controlNetButtonImToIm.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'img2img', 0)}")
-				controlNetButtonTxToIm = gr.Button(value="Send to ControlNet #0 (txt2img -> img2img)", elem_id="sendto_controlnet_button_tx_to_im")
-				controlNetButtonTxToIm.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'img2img', 0)}")
-				controlNetButtonImToTx = gr.Button(value="Send to ControlNet #0 (img2img -> txt2img)", elem_id="sendto_controlnet_button_im_to_tx")
-				controlNetButtonImToTx.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'txt2img', 0)}")
-				return [controlNetButtonTxToTx, controlNetButtonImToIm, controlNetButtonTxToIm, controlNetButtonImToTx]
+def on_after_component(component, **kwargs):
+	global tabId
+	controlNetMaxUnits = getattr(opts, 'control_net_max_models_num', 0)
+	for extension in extensions.active():
+		if "controlnet" in extension.name:
+			currentElement = kwargs.get("elem_id")
+			if currentElement == "extras_tab" and tabId in ["img2img_gallery", "txt2img_gallery"]:
+				with gr.Column():
+					with gr.Row():
+						if tabId == "txt2img_gallery":
+							controlNetButtonTxToTx = gr.Button(value="Send to ControlNet #0", elem_id="sendto_controlnet_button_tx_to_tx")
+							controlNetButtonTxToTx.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'txt2img', 0)}")
+							controlNetButtonTxToIm = gr.Button(value="Send to img2img ControlNet #0", elem_id="sendto_controlnet_button_tx_to_im")
+							controlNetButtonTxToIm.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'img2img', 0)}")
+						elif tabId == "img2img_gallery":
+							controlNetButtonImToIm = gr.Button(value="Send to ControlNet #0", elem_id="sendto_controlnet_button_im_to_im")
+							controlNetButtonImToIm.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'img2img', 0)}")
+							controlNetButtonImToTx = gr.Button(value="Send to txt2img ControlNet #0", elem_id="sendto_controlnet_button_im_to_tx")
+							controlNetButtonImToTx.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'txt2img', 0)}")
+				tabId = ""
+			elif currentElement in ["img2img_gallery", "txt2img_gallery"]:
+				tabId = currentElement
+tabId = ""
+script_callbacks.on_after_component(on_after_component)
