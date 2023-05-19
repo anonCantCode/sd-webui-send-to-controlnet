e82b55ad39c9b421ccaaf01eb5de77c724d30a07
anonCantCode <133663594+anonCantCode@users.noreply.github.com>
2023-05-19 11:15:32 +0530

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -12,25 +12,24 @@ class Script(scripts.Script):
 		return scripts.AlwaysVisible
 
 def on_after_component(component, **kwargs):
-	
-	element = kwargs.get("elem_id")
-	print(element)
-	
-	executed = False #prevent multiple buttons
+	executed = False
+	tabId = ""
 	for extension in extensions.active():
 		if "controlnet" in extension.name and not executed:
-			element = kwargs.get("elem_id")
+			currentElement = kwargs.get("elem_id")
 			executed = True
-			if element == "image_buttons_txt2img":
-				print(element)
+			if currentElement == "extras_tab" and tabId == "txt2img_gallery":
 				controlNetButtonTxToTx = gr.Button(value="Send to ControlNet #0", elem_id="sendto_controlnet_button_tx_to_tx")
 				controlNetButtonTxToTx.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'txt2img', 0)}")
 				controlNetButtonTxToIm = gr.Button(value="Send to img2img ControlNet #0", elem_id="sendto_controlnet_button_tx_to_im")
 				controlNetButtonTxToIm.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'img2img', 0)}")
-			elif element == "image_buttons_img2img":
-				print(element)
+				tabId = ""
+			elif currentElement == "extras_tab" and tabId == "img2img_gallery":
 				controlNetButtonImToIm = gr.Button(value="Send to ControlNet #0", elem_id="sendto_controlnet_button_im_to_im")
 				controlNetButtonImToIm.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'img2img', 0)}")
 				controlNetButtonImToTx = gr.Button(value="Send to txt2img ControlNet #0", elem_id="sendto_controlnet_button_im_to_tx")
 				controlNetButtonImToTx.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'txt2img', 0)}")
+				tabId = ""
+			elif currentElement in ["img2img_gallery", "txt2img_gallery]:
+				tabId = currentElement
 script_callbacks.on_after_component(on_after_component)
