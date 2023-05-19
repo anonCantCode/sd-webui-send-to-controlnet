c7211744b93e3ad09275a6fc626c76884c069699
anonCantCode <133663594+anonCantCode@users.noreply.github.com>
2023-05-19 09:37:03 +0530

    Update main.py: Attempt no. 2 to fix button positions

--- scripts/main.py
+++ scripts/main.py
@@ -16,12 +16,13 @@ class Script(scripts.Script):
 	def ui(self, is_img2img):
 		for extension in extensions.active():
 			if "controlnet" in extension.name:
-				controlNetButtonTxToTx = gr.Button(value="Send to ControlNet #0 (txt2img -> txt2img)", elem_id="sendto_controlnet_button_tx_to_tx")
-				controlNetButtonTxToTx.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'txt2img', 0)}")
-				controlNetButtonImToIm = gr.Button(value="Send to ControlNet #0 (img2img -> img2img)", elem_id="sendto_controlnet_button_im_to_im")
-				controlNetButtonImToIm.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'img2img', 0)}")
-				controlNetButtonTxToIm = gr.Button(value="Send to ControlNet #0 (txt2img -> img2img)", elem_id="sendto_controlnet_button_tx_to_im")
-				controlNetButtonTxToIm.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'img2img', 0)}")
-				controlNetButtonImToTx = gr.Button(value="Send to ControlNet #0 (img2img -> txt2img)", elem_id="sendto_controlnet_button_im_to_tx")
-				controlNetButtonImToTx.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'txt2img', 0)}")
-				return [controlNetButtonTxToTx, controlNetButtonImToIm, controlNetButtonTxToIm, controlNetButtonImToTx]
+				with gr.Column():
+					controlNetButtonTxToTx = gr.Button(value="Send to ControlNet #0 (txt2img -> txt2img)", elem_id="sendto_controlnet_button_tx_to_tx")
+					controlNetButtonTxToTx.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'txt2img', 0)}")
+					controlNetButtonImToIm = gr.Button(value="Send to ControlNet #0 (img2img -> img2img)", elem_id="sendto_controlnet_button_im_to_im")
+					controlNetButtonImToIm.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'img2img', 0)}")
+					controlNetButtonTxToIm = gr.Button(value="Send to ControlNet #0 (txt2img -> img2img)", elem_id="sendto_controlnet_button_tx_to_im")
+					controlNetButtonTxToIm.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'img2img', 0)}")
+					controlNetButtonImToTx = gr.Button(value="Send to ControlNet #0 (img2img -> txt2img)", elem_id="sendto_controlnet_button_im_to_tx")
+					controlNetButtonImToTx.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'txt2img', 0)}")
+					return [controlNetButtonTxToTx, controlNetButtonImToIm, controlNetButtonTxToIm, controlNetButtonImToTx]
