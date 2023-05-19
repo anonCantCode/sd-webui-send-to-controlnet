94df2dec10094624c9f5b09ac4c51fdd80519026
anonCantCode <133663594+anonCantCode@users.noreply.github.com>
2023-05-19 10:41:39 +0530

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -17,13 +17,13 @@ def on_after_component(component, **kwargs):
 		if "controlnet" in extension.name and not executed:
 			element = kwargs.get("elem_id")
 			executed = True
-			if element == "txt2img_gallery":
+			if element == "image_buttons_txt2img":
 				with gr.Row():
 					controlNetButtonTxToTx = gr.Button(value="Send to ControlNet #0", elem_id="sendto_controlnet_button_tx_to_tx")
 					controlNetButtonTxToTx.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'txt2img', 0)}")
 					controlNetButtonTxToIm = gr.Button(value="Send to img2img ControlNet #0", elem_id="sendto_controlnet_button_tx_to_im")
 					controlNetButtonTxToIm.click(fn=None, _js="(i) => {sendImageToControlNet('txt2img', 'img2img', 0)}")
-			elif element == "img2img_gallery":
+			elif element == "image_buttons_img2img":
 				with gr.Row():
 					controlNetButtonImToIm = gr.Button(value="Send to ControlNet #0", elem_id="sendto_controlnet_button_im_to_im")
 					controlNetButtonImToIm.click(fn=None, _js="(i) => {sendImageToControlNet('img2img', 'img2img', 0)}")
